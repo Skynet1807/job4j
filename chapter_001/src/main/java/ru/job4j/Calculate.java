0857f8d8923c36c911d6a28cec7031805ad7b20b
Skynet1807 <bazhenov36@gmail.com>
2018-05-08 14:50:44 +0400

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
-package ru.job4j.calculate;  
-
-
+/**
+* Package for calculate task.
+*
+* @author Vyacheslav Bazhenov (bazhenov36@gmail.com)
+* @version 0.1
+* @since 8.5.18
+*/
+package ru.job4j.calculate; 
+/** 
+* Main
+*@param args-args.
+*/
 public class Calculate {  
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vyacheslav Bazhenov (bazhenov36@gmail.com)
+* @version 0.1
+* @since 8.5.18
+*/
+package ru.job4j;

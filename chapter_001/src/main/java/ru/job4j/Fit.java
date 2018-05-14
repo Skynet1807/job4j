d96e767b614d0a6e6b19d2dd7b9da91a0bb75bd7
Skynet1807 <bazhenov36@gmail.com>
2018-05-14 15:13:00 +0400

    3.2. Конвертер валюты. [#41583][#52903]

--- chapter_001/src/main/java/ru/job4j/Fit.java
+++ chapter_001/src/main/java/ru/job4j/Fit.java
@@ -4,14 +4,17 @@ package ru.job4j;
  * Программа расчета идеального веса.
  */
 public class Fit {
- double a  =  1.15;
+    // CONST константа используемая в формуле для вычисления идеального веса.
+    private static final double CONST  =  1.15;
+
     /**
      * Идеальный вес для мужщины.
      * @param height Рост.
      * @return идеальный вес.
      */
     public double manWeight(double height) {
-        return (height - 100) * a;
+        return (height - 100) * CONST;
+
     }
 
     /**
@@ -20,7 +23,7 @@ public class Fit {
      * @return идеальный вес.
      */
     public double womanWeight(double height) {
-        return (height - 110) * a;
+        return (height - 110) * CONST;
     }
     public static void main(String[] args) {
         Fit fit = new Fit();
